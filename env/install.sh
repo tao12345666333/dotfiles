a5a948af65dca763e3408e9f6108ac6ebcb0222d
ZhangJintao <tao12345666333@163.com>
2015-09-22 23:53:34 +0800

    add env need complete

---
+++ env/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#install vim
+sudo apt-get install python vim exuberant-ctags git
+sudo easy_install pip
+sudo pip install dbgp vim-debug pep8 flake8 pyflakes isort
+wget https://raw.githubusercontent.com/tao12345666333/vim/master/vimrc -O $HOME/.vimrc
+vim -E -u $HOME/.vimrc +qall
+
+
+#install zsh and use oh-my-zsh
+sudo apt-get install zsh
+sudo apt-get install curl
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
