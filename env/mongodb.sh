4d302d93d83d6f3f83ca6ee3405f624c3c9504d6
ZhangJintao <zhangjintao9020@gmail.com>
2016-05-31 12:05:22 +0800

    add mongodb

---
+++ env/mongodb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install debian-archive-keyring
+sudo apt-key update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=3.0.7 mongodb-org-server=3.0.7 mongodb-org-shell=3.0.7 mongodb-org-mongos=3.0.7 mongodb-org-tools=3.0.7
