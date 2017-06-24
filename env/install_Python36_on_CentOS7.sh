80ae7c7ca9ba786638cc4b5cbad0d77f7f123c49
TaoBeier <zhangjintao9020@gmail.com>
2017-06-24 23:54:47 +0800

    add using sourcecode install Python36  on CentOS 7 script

---
+++ env/install_Python36_on_CentOS7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# author: TaoBeier
+yum -y install wget gcc make  zlib-devel readline-devel  bzip2-devel ncurses-devel sqlite-devel gdbm-devel xz-devel tk-devel 
+wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
+xz -d Python-3.6.1.tar.xz
+tar -xvf Python-3.6.1.tar
+cd Python-3.6.1
+./configure --prefix=/usr/local/python3.6 --enable-optimizations
+make
+make install
